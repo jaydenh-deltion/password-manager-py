9d9c28865ae1ea59d2a048ba9d8a12889533979a
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-12-02 15:21:26 +0100

    css

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 # waar de gebruiken heen kan gaan
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # home page
 def home():
-    return render_template('home.html')
+    return render_template('login.html')
