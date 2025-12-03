3a2a4fdb148d2bcda040da4a45946862e67c6660
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-12-03 12:38:19 +0100

    login en signup

--- website/auth.py
+++ website/auth.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login') # login page
+@auth.route('/login')
 def login():
     return render_template('login.html')
 
-@auth.route('/signup') # signup page
+@auth.route('/signup')
 def signup():
-    return render_template('sign_rup.html')
+    return render_template('sign_up.html')
--- website/views.py
+++ website/views.py
@@ -1,18 +1,12 @@
 # waar de gebruiken heen kan gaan
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/') # login page
+@views.route('/')
 def login():
     return render_template('login.html')
 
-@views.route('/dashboard') # dashboard page
+@views.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
-
-@views.route('/signup') # signup page
-def signup():
-    return render_template('sign_rup.html')
-
-
