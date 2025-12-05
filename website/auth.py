8a66cd7cb5872110ca94a68a1fc93bf074826407
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-12-05 09:29:05 +0100

    ...

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import login_required, current_user, logout_user, login_user, UserMixin, LoginManager
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +12,5 @@ def signup():
     return render_template('sign_up.html')
 
 @auth.route('/dashboard')
-@login_required
 def dashboard():
     return render_template('dashboard.html')
---
+++ website/models.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+def create_database():
+    conn = sqlite3.connect('passwords.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (
+                         id INTEGER PRIMARY KEY,
+                         service_name TEXT NOT NULL,
+                         username TEXT NOT NULL,
+                         password TEXT NOT NULL)''')
+    conn.commit()
+    conn.close()
