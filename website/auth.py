dd20b15cfc11907f92b0f304adae23094b786447
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-12-19 15:25:21 +0100

    db

--- website/auth.py
+++ website/auth.py
@@ -9,6 +9,21 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template('login.html')
 
 @auth.route('/logout')
--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 # waar de gebruiken heen kan gaan
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import User, Password
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST']) 
 @login_required
 def home():
-    return render_template('dashboard.html')
+    
+    return render_template('dashboard.html', user=current_user)
